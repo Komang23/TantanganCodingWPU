9fc8ccc818aff33fe54b1115d8aa680180715815
Komang23 <113251487+Komang23@users.noreply.github.com>
2024-03-20 01:29:44 +0800

    Update description

--- #1grow.js
+++ #1grow.js
@@ -1,11 +1,22 @@
+//#1
+//https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/javascript
+
+
+// CARA PERTAMA
+
+// function grow(x) {
+//     let result = x[0];
+//     for (let i = 1; i < x.length; i++) {           
+//         result = result * x[i];
+//     }
+//     return result;
+// }
+
+// CARA KE DUA
+
 function grow(x) {
-    let result = x[0];
-    for (let i = 1; i < x.length; i++) {
-        result = result * x[i];
-    }
-    return result;
+    const result = x.reduce((acc, curr) => acc * curr, 1);
+    return result
 }
-
-
-console.info(grow([1, 2, 3, 4]));
+console.log(grow([1, 2, 3, 4]));
 
