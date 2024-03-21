c22fbe29ec68bc69729a5e10696376d9b16ffa95
Komang23 <113251487+Komang23@users.noreply.github.com>
2024-03-21 11:10:11 +0800

    soal tantangan code wars wpu

---
+++ #2reverseSeq.js
@@ -0,0 +1,15 @@
+
+//Cara Pertama
+// const reverseSeq = n => {
+//     const result = [];
+//     for (let i = n; i >= 1; i--) {
+//         result.push(i);
+//     }
+//     return result;
+// };
+
+const reverseSeq = (n) => {
+    return Array(n).fill().map((el, i) => i + 1).reverse();
+}
+
+console.log(reverseSeq(5));
