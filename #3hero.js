ae03e50ef05bd90ffbab3ba8517a09e05b981de6
Komang23 <113251487+Komang23@users.noreply.github.com>
2024-03-27 21:54:57 +0800

    soal ke 3

---
+++ #3hero.js
@@ -0,0 +1,15 @@
+//#3
+//https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+
+// CARA PERTAMA
+// function hero(bullets, dragons) {
+//     if (bullets / 2 >= dragons) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+//CARA KE DUA
+const hero = (bullets, dragons) => bullets / 2 >= dragons;
